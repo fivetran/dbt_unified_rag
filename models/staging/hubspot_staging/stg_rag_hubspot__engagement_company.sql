99af7a8ee59e60eccef9fee8f0438ea3c303b129
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2025-02-19 16:15:27 -0800

    typo

--- models/staging/hubspot_staging/stg_rag_hubspot__engagement_company.sql
+++ models/staging/hubspot_staging/stg_rag_hubspot__engagement_company.sql
@@ -12,7 +12,7 @@ fields as (
         {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_rag_hubspot__engagement_company_base')),
-                staging_columns=get_hubspot_engagement_company_colusmns()
+                staging_columns=get_hubspot_engagement_company_columns()
             )
         }}
 
